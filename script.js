5a611c6de1980386f42bd20169e5dfc5decfeb24
DatLmao <datnguyendat2006@gmail.com>
2025-08-08 15:46:24 +0700

    x

---
+++ script.js
@@ -0,0 +1,11 @@
+
+        const navbarLinks = document.querySelectorAll('.navbar__link');
+        navbarLinks.forEach(link => {
+            link.addEventListener('click', function(e) {
+                e.preventDefault(); 
+                navbarLinks.forEach(item => {
+                    item.classList.remove('navbar__link--active');
+                });
+                this.classList.add('navbar__link--active');
+            });
+        });
