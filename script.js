21fad17f367087dffe2e657a639cc269523db7eb
DatLmao <datnguyendat2006@gmail.com>
2025-08-08 21:54:58 +0700

    service

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-
-        const navbarLinks = document.querySelectorAll('.navbar__link');
-        navbarLinks.forEach(link => {
-            link.addEventListener('click', function(e) {
-                e.preventDefault(); 
-                navbarLinks.forEach(item => {
-                    item.classList.remove('navbar__link--active');
-                });
-                this.classList.add('navbar__link--active');
+    const navbarLinks = document.querySelectorAll('.navbar__link');
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); 
+            navbarLinks.forEach(item => {
+                item.classList.remove('navbar__link--active');
             });
+            this.classList.add('navbar__link--active');
         });
+    });
